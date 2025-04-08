0e246cf8509aa3284743025736be92c5306347e5
Clément Desberg <clement@desberg.fr>
2025-04-08 11:07:18 +0200

    commited on 20250408

---
+++ model/map.cpp
@@ -0,0 +1,17 @@
+#ifndef MAP_CPP
+#define MAP_CPP
+#define MAP_HEIGHT 10
+#define MAP_WIDTH 15
+
+#include <iostream>
+#include <vector>
+
+class Map{
+    public:
+    Map(){
+        // Create an empty map
+        
+    }
+};
+
+#endif
