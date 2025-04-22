3cce051d662874512d315ce81eefd507ac259cfb
Clément Desberg <clement@desberg.fr>
2025-04-22 12:02:17 +0200

    20250422 updated tile type

--- model/map.cpp
+++ model/map.cpp
@@ -1,5 +1,9 @@
 #include "map.hpp"
 
-Map::Map(){
-    
-}
+Map::Map(int height, int width):
+height(height), width(width), data(height, std::vector<Tile>(width, Tile()))
+{}
+
+Tile::Tile():
+owner_id(-1), has_wall(false), character_type(-1)
+{}
--- model/map.hpp
+++ model/map.hpp
@@ -1,21 +1,27 @@
 #ifndef MAP_CPP
 #define MAP_CPP
-#define MAP_HEIGHT 10
-#define MAP_WIDTH 15
 
 #include <iostream>
 #include <vector>
 
 class Tile{
+    private:
+    int owner_id;
+    bool has_wall;
+    int character_type;
 
+    public:
+    Tile();
 };
 
 class Map{
     private:
     std::vector<std::vector<Tile>> data;
+    int height;
+    int width;
 
     public:
-    Map();
+    Map(int height, int width);
 };
 
 #endif
