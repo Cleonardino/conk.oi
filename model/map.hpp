385a6e75e6216c7582a0f012f89055d46a30547d
Clément Desberg <clement@desberg.fr>
2025-04-24 13:44:44 +0200

    20250424 merge

--- model/map.hpp
+++ model/map.hpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #ifndef MAP_HPP
 #define MAP_HPP
 
@@ -70,74 +69,4 @@ class Map{
     friend std::ostream& operator<<(std::ostream& os, const Map& map);
 };
 
-=======
-#ifndef MAP_HPP
-#define MAP_HPP
-
-#include <iostream>
-#include <vector>
-
-enum TileType {
-    Ocean,
-    Land,
-    Forest
-};
-
-enum CharacterType {
-    Empty,
-    Peasant,
-    Soldier,
-    Knight
-};
-
-enum BuildingType {
-    Wild,
-    Town,
-    Fortress
-};
-
-class Tile{
-    private:
-    TileType tile_type;
-    int owner_id;
-    bool has_wall;
-    BuildingType building_type;
-    CharacterType character_type;
-
-    public:
-
-    Tile(TileType tile_type_, int owner_id_, bool has_wall_,
-        BuildingType building_type_, CharacterType character_type_);
-
-    static Tile default_Tile();
-
-    void update_tile(TileType tile_type_, int owner_id_, bool has_wall_,
-        BuildingType building_type_, CharacterType character_type_);
-
-    TileType get_type();
-    int get_owner();
-    bool get_wall();
-    BuildingType get_building();
-    CharacterType get_character();
-
-    friend std::ostream& operator<<(std::ostream& os, const Tile& tile);
-};
-
-class Map{
-    private:
-    std::vector<std::vector<Tile>> data;
-    int height;
-    int width;
-
-    public:
-
-    Map(int height_, int width_);
-    Tile get_Tile(int row, int column);
-    void set_Tile(int row, int column, TileType tile_type, int owner_id,
-        bool has_wall, BuildingType building_type, CharacterType character_type);
-
-    friend std::ostream& operator<<(std::ostream& os, const Map& map);
-};
-
->>>>>>> 6f0d117f9540f1dc61fd8812661fd93b0b8cf00f
 #endif
